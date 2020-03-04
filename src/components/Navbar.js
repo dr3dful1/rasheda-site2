5a5b8b2d5ce482cf878bceadfbc63774ad8b9fe9
dr3dful1 <karmstr2@gmail.com>
2020-03-04 15:24:33 -0800

    logo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,9 @@ import {Nav, NavItem,Navbar,Badge} from 'react-bootstrap'
 function MainNavbar() {
     return (
         <div className="mainNav">
+            <div className="logoDiv">
+                <img src="lawfirmlogo.jpg" alt="Italian Trulli" className="navLogo " />
+            </div>
       
         </div>
     )
